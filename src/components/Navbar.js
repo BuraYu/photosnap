e1948e80e016d2d7b699719a6b90ee57f97b9d28
BuraYu <burak.yueksel@hotmail.com>
2024-07-25 23:00:34 +0200

    add Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,22 @@
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="navbar__container">
+      <span className="navbar__logo">Photosnap</span>
+      <div className="navbar__navigation-container">
+        <ul className="navbar__navigation-list">
+          <li className="navbar__navigation-item">
+            <a href="#" className="navbar__navigation-link">Stories</a>
+          </li>
+          <li className="navbar__navigation-item">
+            <a href="#" className="navbar__navigation-link">Features</a>
+          </li>
+          <li className="navbar__navigation-item">
+            <a href="#" className="navbar__navigation-link">Pricing</a>
+          </li>
+        </ul>
+      </div>
+      <span className="navbar__label">Get an invite</span>
+    </nav>
   );
 };
 

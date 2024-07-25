a486b36607d14c2c002b84e8863ccf83e6647290
BuraYu <burak.yueksel@hotmail.com>
2024-07-25 22:29:30 +0200

    add skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Story from "./components/Story";
+import Info from "./components/Info";
+import Footer from "./components/Footer";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Section />
+      <Section />
+      <Section />
+      <Story />
+      <Info />
+      <Footer />
     </div>
   );
 }

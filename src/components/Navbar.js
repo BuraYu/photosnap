462eb87115aa5858cc6a3e159e17af9fa6bb55dd
BuraYu <burak.yueksel@hotmail.com>
2024-07-27 23:30:15 +0200

    add css for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,27 @@ const Navbar = () => {
       <div className="navbar__navigation-container">
         <ul className="navbar__navigation-list">
           <li className="navbar__navigation-item">
-            <a href="#" className="navbar__navigation-link">Stories</a>
+            <a href="#" className="navbar__navigation-link">
+              Stories
+            </a>
           </li>
           <li className="navbar__navigation-item">
-            <a href="#" className="navbar__navigation-link">Features</a>
+            <a href="#" className="navbar__navigation-link">
+              Features
+            </a>
           </li>
           <li className="navbar__navigation-item">
-            <a href="#" className="navbar__navigation-link">Pricing</a>
+            <a href="#" className="navbar__navigation-link">
+              Pricing
+            </a>
           </li>
         </ul>
       </div>
-      <span className="navbar__label">Get an invite</span>
+      <span className="navbar__label">
+        <a href="#" className="navbar__lable-link">
+          Get an invite
+        </a>
+      </span>
     </nav>
   );
 };

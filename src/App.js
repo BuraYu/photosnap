629473133a7de97b35f46940798593a7767b8d50
BuraYu <burak.yueksel@hotmail.com>
2024-08-02 22:40:44 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Story from "./components/Story";
-import Info from "./components/Info";
-import Footer from "./components/Footer";
-import Section from "./components/Section";
+import { Route, Routes } from "react-router-dom";
+import Home from './Pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Section />
-      <Section />
-      <Section />
-      <Story />
-      <Info />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import Story from "../components/Story";
+import Info from "../components/Info";
+import Footer from "../components/Footer";
+import Section from "../components/Section";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Section />
+      <Section />
+      <Section />
+      <Story />
+      <Info />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
